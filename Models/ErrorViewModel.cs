0d0220c79f2b84b3bd7f908e944da1e9be67f376
DESKTOP-DQCFMF2\syntagma <walter.davalos@syntagma.com.ar>
2018-01-05 15:22:56 -0300

    proj

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectEjemplo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
